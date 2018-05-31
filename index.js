sdd/serverless-apig-s3

commit 87f48c0ba6b4402047f6940c8f395e3fae8ca55b

fix this.stage to this.options.stage

index.js
@@ -50,9 +50,9 @@ module.exports = class ServerlessApigS3 extends ServerlessAWSPlugin {
         );
     }
 
-    updateIamRoleAndPolicyNames(roleResource, stage) {
-        roleResource[ "Properties" ][ "RoleName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "RoleName" ] + "_" + stage;
-        roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] + "_" + stage;
+    updateIamRoleAndPolicyNames(roleResource) {
+        roleResource[ "Properties" ][ "RoleName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "RoleName" ] + "_" + this.options.stage;
+        roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] + "_" + thiis.options.stage;
         return roleResource
     }
 

commit 4dca8148f169532dd7b39c9009a752e2f3128af7

fix a typo introduced in commit 87f48c0ba6b4402047f6940c8f395e3fae8ca55b

index.js
@@ -52,7 +52,7 @@ module.exports = class ServerlessApigS3 extends ServerlessAWSPlugin {
 
     updateIamRoleAndPolicyNames(roleResource) {
         roleResource[ "Properties" ][ "RoleName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "RoleName" ] + "_" + this.options.stage;
-        roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] + "_" + thiis.options.stage;
+        roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] = this.stackName + "_" + roleResource[ "Properties" ][ "Policies" ][ 0 ][ "PolicyName" ] + "_" + this.options.stage;
         return roleResource
     }
 